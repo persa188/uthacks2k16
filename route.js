f92317676ba980b60b8f8aea79335143a18abaf5
evm <emagharian@hotmail.com>
2016-01-24 01:24:33 -0500

    shitzzz beeen done

--- route.js
+++ route.js
@@ -1,13 +1,26 @@
-
+//ExpressJS Init
 var express = require('express');
 var app = express();
 
+//DB Init
+var db = require("./db_interface.js");
+
+
+//Routes
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+	res.send('Hello World!');
 });
+
+app.get('/helloworld', function (req, res) {
+	res.send('SURPRISE MOTHERTFUCKER');
+});
+
+app.post('/login', function(req, res) {
+	res.redirect("/helloworld");
+})
 
 app.use(express.static('public'));
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+	console.log('Example app listening on port 3000!');
 });
